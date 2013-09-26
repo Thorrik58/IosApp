c19a8dd508854b5b23ab89f7174ba0f2c00cafce
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2013-09-26 02:25:43 +0000

    Revert "Revert "git reverted my stupid revert""
    This reverts commit 594131f75974cffcfa74d19d2aba909dc2ddc9b8.

--- iOS App Dev/Player.h
+++ iOS App Dev/Player.h
@@ -8,12 +8,12 @@
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 
-@interface Player : CCSprite
+@interface Player : CCPhysicsSprite
 {
-    
+    ChipmunkSpace *_space;
 }
 
-- (id)initWithPosition:(CGPoint)position;
+- (id) initWithSpace:(ChipmunkSpace *)space position:(CGPoint)position;
 - (void)fly; //veit ekkert hvort eg muni thurfa thetta
 
 @end
--- iOS App Dev/Player.m
+++ iOS App Dev/Player.m
@@ -9,14 +9,54 @@
 
 @implementation Player
 
-- (id)initWithPosition:(CGPoint)position
+- (id) initWithSpace:(ChipmunkSpace *)space position:(CGPoint)position;
 {
-    self = [super initWithFile:@"kirby.png"];
+    self = [super initWithFile:@"pony.png"];
     if (self)
     {
-        self.position = position;
+        _space = space;
+        
+        if (space != nil)
+        {
+            //TODO: Look into if I should perhaps do this with auto geometry.
+            
+            /*        
+             //Not working!!
+             NSURL *url = [[NSBundle mainBundle] URLForResource:@"pony" withExtension:@"png"];
+             
+             ChipmunkImageSampler *sampler = [ChipmunkImageSampler samplerWithImageFile:url isMask:NO];
+             ChipmunkPolylineSet *contour = [sampler marchAllWithBorder:NO hard:YES];
+             ChipmunkPolyline *line = [contour lineAtIndex:0];
+             ChipmunkPolyline *simpleLine = [line simplifyCurves:1];
+             
+             ChipmunkBody *ponyBody = [ChipmunkBody bodyWithMass:10000.0f andMoment:1000000.0f];
+             NSArray *ponyShapes = [simpleLine asChipmunkSegmentsWithBody:ponyBody radius:0 offset:cpvzero];
+             for (ChipmunkShape *shape in ponyShapes)
+             {
+             [_space addShape:shape];
+             //self.chipmunkBody = ponyBody;
+             }
+             */
+            
+            CGSize size = self.textureRect.size;
+            cpFloat mass = size.width * size.height;
+            cpFloat moment = cpMomentForBox(mass, size.width, size.height);
+            
+            ChipmunkBody *body = [ChipmunkBody bodyWithMass:mass andMoment:moment];
+            body.pos = position;
+            ChipmunkShape *shape = [ChipmunkPolyShape boxWithBody:body width:size.width height:size.height];
+            
+            //Add to space
+            [_space addBody:body];
+            [_space addShape:shape];
+            
+            //add to physics sprite
+            
+            self.chipmunkBody = body;
+            
+        }
     }
-return self;
+    return self;
 }
 
 - (void)fly
