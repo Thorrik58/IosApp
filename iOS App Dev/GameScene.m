7a56549f953b9b67b208b1c02c9eb5e56ea6a026
Þorvarður Örn Einarsson <Thorrik58@gmail.com>
2013-09-28 20:12:33 +0000

    Restart function with GameOver and WinGame

--- iOS App Dev/GameScene.m
+++ iOS App Dev/GameScene.m
@@ -17,6 +17,66 @@
 
 #pragma mark - Initilization
 
+
+//HUD and restart
++ (id)scene {
+    CCScene *scene = [CCScene node];
+    
+    GameScene *layer = [GameScene node];
+    [scene addChild:layer];
+    
+    return scene;
+}
+
+- (void)restartTapped:(id)sender {
+    
+    // Reload the current scene
+    CCScene *scene = [GameScene scene];
+    [[CCDirector sharedDirector] replaceScene:[CCTransitionZoomFlipX transitionWithDuration:0.5 scene:scene]];
+    
+}
+
+- (void)showRestartMenu:(BOOL)won {
+    
+    CGSize winSize = [CCDirector sharedDirector].winSize;
+    
+    NSString *message;
+    if (won) {
+        message = @"You win!";
+    } else {
+        message = @"You lose!";
+    }
+    
+    CCLabelBMFont *label;
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        label = [CCLabelBMFont labelWithString:message fntFile:@"Arial-hd.fnt"];
+    } else {
+        label = [CCLabelBMFont labelWithString:message fntFile:@"Arial.fnt"];
+    }
+    label.scale = 0.1;
+    label.position = ccp(winSize.width/2, winSize.height * 0.6);
+    [self addChild:label];
+    
+    CCLabelBMFont *restartLabel;
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        restartLabel = [CCLabelBMFont labelWithString:@"Restart" fntFile:@"Arial-hd.fnt"];
+    } else {
+        restartLabel = [CCLabelBMFont labelWithString:@"Restart" fntFile:@"Arial.fnt"];
+    }
+    
+    CCMenuItemLabel *restartItem = [CCMenuItemLabel itemWithLabel:restartLabel target:self selector:@selector(restartTapped:)];
+    restartItem.scale = 0.1;
+    restartItem.position = ccp(winSize.width/2, winSize.height * 0.4);
+    
+    CCMenu *menu = [CCMenu menuWithItems:restartItem, nil];
+    menu.position = CGPointZero;
+    [self addChild:menu z:10];
+    
+    [restartItem runAction:[CCScaleTo actionWithDuration:0.5 scale:1.0]];
+    [label runAction:[CCScaleTo actionWithDuration:0.5 scale:1.0]];
+    
+}
+
 - (id)init
 {
     self = [super init];
@@ -61,7 +121,6 @@
     }
     return self;
 }
-
 
 - (void) setupPhysicsWorld
 {
@@ -168,6 +227,8 @@
         //_backgroundNode.position = ccpAdd(_backgroundNode.position, ccpMult(backgroundScrollVel, delta));
         _backgroundNode.position = ccp(-(_player.position.x - (_winSize.width / 2)),0);
     }
+    
+       
     
     //NSLog(@"Pos: %f",_distanceScore);
 
